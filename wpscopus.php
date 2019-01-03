c3886f537f96f4f783d18e824c5980326fc4b507
the16thpythonist <jonseb1998@gmail.com>
2019-01-03 17:45:19 +0100

    initial commit

---
+++ wpscopus.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: WpScopus
+ * Plugin URI: https://github.com/the16thpythonist/wp-scopus-plugin
+ * Description: Wordpress Scopus science
+ * Author: Jonas Teufel
+ * Version: 0.0.0.0
+ * Author URI: google.com
+ * License: GPLv2 or later
+ */
+
+namespace WpScopus;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use the16thpythonist\Wordpress\Scopus\WpScopus;
+
+WpScopus::register('a', 'a');
